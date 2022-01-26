5567cd36ebdef4aae1ed90c70fea8331fe6d4c1f
DhakadCoder <govindkr.dhakar.che19@itbhu.ac.in>
2022-01-26 22:43:32 +0530

    first commit

--- index.js
+++ index.js
@@ -164,6 +164,9 @@ app.use((err, req, res, next) => {
     const { statusCode = 404, message = 'something went wrong' } = err;
     res.status(statusCode).render('error', { err });
 })
-app.listen(3000, () => {
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Noice")
 })
